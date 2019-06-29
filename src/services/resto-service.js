c63220eda72717f37d19f1dc83852a21b0ca8c6e
Miguel Osorio <mial.osorio@gmail.com>
2019-06-30 00:19:48 +0200

    вывел данные списком объектов

--- src/services/resto-service.js
+++ src/services/resto-service.js
@@ -1,10 +1,10 @@
 export default class RestoService {
-    constructor(){
-        this._base = `http://localhost:3000/`;
-      }
+  constructor() {
+    this._base = `http://localhost:3000/`;
+  }
 
-    getMenuItems = async () => {
-        const res = await fetch(`${this._base}menu`);
-        return res.json()
-      }
+  getMenuItems = async () => {
+    const res = await fetch(`${this._base}menu`)
+    return res.json().then(data => data.map(item => console.log(item)))
+  }
 }
