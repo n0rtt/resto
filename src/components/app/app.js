5e0fc0989dfe87f2ddf11accdcd570480ace8413
Miguel Osorio <mial.osorio@gmail.com>
2019-06-29 21:15:09 +0200

    lesson 14

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,17 +1,33 @@
 import React from 'react';
-import {MainPage, CartPage} from '../pages';
+import { MainPage, CartPage } from '../pages';
 import AppHeader from '../app-header';
-
+import WithRestoService from '../hoc'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Background from './food-bg.jpg';
 
-const App = () => {
+
+const App = ({ RestoService }) => {
+
+    console.log(RestoService.getMenuItems())
+
+    const NoMatch = ({ location }) => (
+        <div>
+            <span>Page not found.</span>
+        </div>
+    )
+
     return (
-        <div style={{background: `url(${Background}) center center/cover no-repeat`}} className="app">
-            <AppHeader total={50}/>
-            <MainPage/>
-            <CartPage/>
+        <div style={{ background: `url(${Background}) center center/cover no-repeat` }} className="app">
+            <Router>
+                <AppHeader total={50} />
+                <Switch>
+                    <Route path='/' exact component={MainPage} />
+                    <Route path='/cart' exact component={CartPage} />
+                    <Route component={NoMatch} />
+                </Switch>
+            </Router>
         </div>
     )
 }
 
-export default App;
+export default WithRestoService()(App);
---
+++ src/services/resto-service.js
@@ -0,0 +1,10 @@
+export default class RestoService {
+    constructor(){
+        this._base = `http://localhost:3000/`;
+      }
+
+    getMenuItems = async () => {
+        const res = await fetch(`${this._base}menu`);
+        return res.json()
+      }
+}
